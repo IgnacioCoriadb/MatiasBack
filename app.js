6666eb702d8196ffc74a6aa8ba359f4f36c88e0a
Ignacio <ignacioCoriadb@gmail.com>
2023-08-04 11:41:26 -0300

    cors , express.json(), express.urlencoded

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const server = express();
 const routes = require('./src/Routes/Index');
+const cors = require('cors');
+
+
+server.use(cors());
+// Middleware para analizar el cuerpo de las solicitudes JSON
+server.use(express.json());
+// Middleware para analizar el cuerpo de las solicitudes de formulario
+server.use(express.urlencoded({ extended: true }));
+
+
 
 server.use('/', routes);
+
+
 
 module.exports = server;
