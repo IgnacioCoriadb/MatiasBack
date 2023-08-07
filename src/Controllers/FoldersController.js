0463e81d8ecd4ece030e993fd0638d210f5a53df
Ignacio <ignacioCoriadb@gmail.com>
2023-08-07 12:27:12 -0300

    deleteFolder controller

--- src/Controllers/FoldersController.js
+++ src/Controllers/FoldersController.js
@@ -1,4 +1,6 @@
 const {Folders,Op} = require("../Models/Folders");
+const {Images,uuidv4} = require("../Models/Images");
+
 const cloudinary = require("../Cloudinary/Cloudinary");
 
 const createFolder = async (subfolder)=>{
@@ -6,13 +8,13 @@ const createFolder = async (subfolder)=>{
     const routeSubFolder = `${folder}/${subfolder}`;
     // Crea la subcarpeta en Cloudinary
     return new Promise((resolve,reject)=>{
-        cloudinary.api.create_folder(routeSubFolder, (error, result) => {
+        cloudinary.api.create_folder(routeSubFolder, async(error, result) => {
             if (error) {
                 console.error('Error al crear la subcarpeta:', error);
                 reject('Error al crear la subcarpeta')
             } else {
                 try{
-                    Folders.create({folderName:subfolder})
+                    await Folders.create({id: uuidv4(),folderName:subfolder})
                     resolve(`Carpeta ${subfolder} creada exitosamente`)
                 }catch(error){
                     console.error('Error al crear la subcarpeta:', error)
@@ -20,7 +22,6 @@ const createFolder = async (subfolder)=>{
                 }
             }
         });
-
     })
 }
 
@@ -41,9 +42,62 @@ const allFolder =async() => {
     }
 }
 
+const deleteFolder = async(idDb)=>{
+    const folder = await Folders.findByPk(idDb);
+    if(folder){
+        const folderName = `imagesMatias/${folder.folderName}`;
+        const portfolio = await Images.findAll({
+            attributes: ['id'], 
+            raw: true, 
+            where:{
+            folderName: folder.folderName
+            }
+        });
+        const idsToDelete = portfolio.map(user => user.id.toString());
+        //delete Folder db
+        await folder.destroy();
+        //delete images in Folder db
+        await Images.destroy({
+            where: {
+            id: {
+                [Op.in]: idsToDelete,
+            },
+            },
+        });
+        //delete cloudinary folder
+        const deleteCloud =()=>{
+            return new Promise((resolve, reject) => {
+            cloudinary.api.delete_resources_by_prefix(folderName, { type: 'upload' }, (error, result) => {
+                if (error) {
+                // console.error('Error al eliminar los recursos:', error);
+                    reject('Error al eliminar las imagenes de la carpeta: ' +folder.folderName);
+                } else {
+                    console.log('Recursos eliminados correctamente:', result);
+                    cloudinary.api.delete_folder(folderName, (error, result) => {
+                        if (error) {
+                        console.error('Error al eliminar la subcarpeta:', error);
+                            reject('Error al eliminar la subcarpeta: '+folder.folderName);
+                        } else {
+                            console.log('Subcarpeta eliminada correctamente:', result);
+                            resolve(`Carpeta ${folder.folderName} eliminada exitosamente`);
+                        }
+                    });
+                }
+            });
+        });
+    };
+    const deleteCloudFolder = await deleteCloud();
+    return deleteCloudFolder;
+    }else{
+      return "No se encontro carpeta"
+    }
+}
+
+
 
 
 module.exports = {
     createFolder,
-    allFolder
+    allFolder,
+    deleteFolder
 }
--- src/Routes/Folders.js
+++ src/Routes/Folders.js
@@ -1,10 +1,6 @@
 const {Router} = require("express");
 const router = Router();
-const {Folders,Op} = require("../Models/Folders");
-const {Images} = require("../Models/Images");
-const cloudinary = require("../Cloudinary/Cloudinary");
-
-const {createFolder,allFolder} =require("../Controllers/FoldersController");
+const {createFolder,allFolder,deleteFolder} =require("../Controllers/FoldersController");
 
 router.post("/createFolder",  async(req, res)=>{
     const { subfolder } = req.body;
@@ -28,63 +24,13 @@ router.get("/allFolders", async(req,res) => {
 router.delete("/deleteFolder/:idDb",async(req, res) => {
     const {idDb} = req.params
     try{
-        const deleteFolder = await deleteFolders(idDb);
-        res.json(deleteFolder);
-    }catch(error){
-        console.log(error)
-        res.json("No se pudo eliminar la carpeta");
+        res.json(await deleteFolder(idDb));
+    }catch(err){
+        console.log("No se pudo ejecutar la funcion deleteFolder " +err);
+        res.json("No se pudo ejecutar la funcion deleteFolder " +err);
     }
 });
 
-const deleteFolders=async(idDb)=>{
-    const folder = await Folders.findByPk(idDb);
-    if(folder){
-        const folderName = `imagesMatias/${folder.folderName}`;
-        const portfolio = await Images.findAll({
-            attributes: ['id'], 
-            raw: true, 
-            where:{
-            folderName: folder.folderName
-            }
-        });
-        const idsToDelete = portfolio.map(user => user.id.toString());
-        //delete Folder db
-        await folder.destroy();
-        //delete images in Folder db
-        await Images.destroy({
-            where: {
-            id: {
-                [Op.in]: idsToDelete,
-            },
-            },
-        });
-        //delete cloudinary folder
-        const deleteCloud =()=>{
-            return new Promise((resolve, reject) => {
-            cloudinary.api.delete_resources_by_prefix(folderName, { type: 'upload' }, (error, result) => {
-                if (error) {
-                // console.error('Error al eliminar los recursos:', error);
-                    reject('Error al eliminar las imagenes de la carpeta: ' +folder.folderName);
-                } else {
-                    console.log('Recursos eliminados correctamente:', result);
-                    cloudinary.api.delete_folder(folderName, (error, result) => {
-                        if (error) {
-                        console.error('Error al eliminar la subcarpeta:', error);
-                            reject('Error al eliminar la subcarpeta: '+folder.folderName);
-                        } else {
-                            console.log('Subcarpeta eliminada correctamente:', result);
-                            resolve(`Carpeta ${folder.folderName} eliminada exitosamente`);
-                        }
-                    });
-                }
-            });
-        });
-    };
-    const deleteCloudFolder = await deleteCloud();
-    return deleteCloudFolder;
-    }else{
-      return "No se encontro carpeta"
-    }
-}
+
 
 module.exports = router;
