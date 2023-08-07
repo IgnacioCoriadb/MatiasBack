7db1c149f26d0034405fed7866d11923d2cf248d
Ignacio <ignacioCoriadb@gmail.com>
2023-08-07 11:41:57 -0300

    delete image

--- src/Routes/Images.js
+++ src/Routes/Images.js
@@ -101,4 +101,41 @@ router.get("/allImage/:folder?", async (req, res) =>{
   }
 })
 
+router.delete("/deleteImage/:idDb",async(req, res) => {
+  const {idDb} = req.params;
+  try{
+    const resutl =await deleteImageDb(idDb);
+    res.json(resutl)
+  }catch(error){
+    console.log("No se pudo eliminar la imagen " +error);
+    res.json("No se pudo eliminar la imagen")
+  }
+})
+
+const deleteImageDb =async (idDb)=>{
+  const image = await Images.findByPk(idDb);
+  if(image){
+    const idCloud = image.dataValues.idCloud;
+    const deleteCloud =await deleteImageCloud(idCloud)
+    await image.destroy();
+    return deleteCloud
+  }else{
+    return "No se encontro imagen"
+  }
+}
+
+const deleteImageCloud = async(idCloud)=>{
+  const imagePublicId =idCloud;
+    return new Promise((resolve, reject) => {
+      cloudinary.uploader.destroy(imagePublicId, (error, result) => {
+      if (error) {
+        console.error('Error al eliminar la imagen:', error);
+        reject('Error al eliminar la imagen');
+      }else{
+        console.log('Imagen eliminada exitosamente:', result);
+        resolve('Imagen eliminada exitosamente');
+      }
+    });
+  })
+}
 module.exports = router;
