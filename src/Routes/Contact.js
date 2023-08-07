d01a84052e7e695a5f16720effdf0392afa892c8
Ignacio <ignacioCoriadb@gmail.com>
2023-08-07 15:55:13 -0300

    route contact

--- src/Routes/Contact.js
+++ src/Routes/Contact.js
@@ -5,8 +5,13 @@ const {contact} = require("../Controllers/ContactController");
 
 router.get("/",async(req, res) => {
     const {name,lastname,email,message} = req.body;
+    try{
+        res.json(await contact(name,lastname,email,message))
+    }catch(err){
+        console.log("No se pudo ejecutar la funcion contact " +err);
+        res.json("No se pudo ejecutar contact " +err);
+    }
 
-    res.json(await contact(name,lastname,email,message))
 })
 
 
