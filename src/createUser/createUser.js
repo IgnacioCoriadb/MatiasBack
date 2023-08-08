538995f43150be31384e716e4e232f1948e671fc
Ignacio <ignacioCoriadb@gmail.com>
2023-08-08 11:23:37 -0300

    create user try catch

--- src/createUser/createUser.js
+++ src/createUser/createUser.js
@@ -4,17 +4,21 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const createUser = async () =>{
-    const user = await User.findOne();
-    if(user){
-        console.log('El usuario ya existe en la base de datos.');       
-    }else{
-        const plainPassword = process.env.PASSWORD_JWT;
-        const plainUser =process.env.USER_JWT;
-        const saltRounds = 10;
-        const hashedPassword = bcrypt.hash(plainPassword, saltRounds);
-        const hashedUser = bcrypt.hash(plainUser,saltRounds);
-        await User.create({user: hashedUser, password: hashedPassword});
-        console.log('Usuario creado exitosamente.');
+    try{
+        const user = await User.findOne();
+        if(user){
+            console.log('El usuario ya existe en la base de datos.');       
+        }else{
+            const plainPassword = process.env.PASSWORD_JWT;
+            const plainUser =process.env.USER_JWT;
+            const saltRounds = 10;
+            const hashedPassword = bcrypt.hash(plainPassword, saltRounds);
+            const hashedUser = bcrypt.hash(plainUser,saltRounds);
+            await User.create({user: hashedUser, password: hashedPassword});
+            console.log('Usuario creado exitosamente.');
+        }
+    }catch(err){
+        console.log("No se pudo ejecutar la consulta createUser " + err)
     }
 }
 
