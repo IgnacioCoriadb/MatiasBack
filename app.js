4bb0628ee5aa48f331eedf4c828b759d4b78cd0f
Icoriadb <icoriadebernardi@gmail.com>
2023-08-31 11:33:36 -0300

    cors

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ server.use(express.json());
 // Middleware para analizar el cuerpo de las solicitudes de formulario
 server.use(express.urlencoded({ extended: true }));
 
-
+server.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://www.longoperrig.com.ar');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+  
 
 server.use('/', routes);
 
