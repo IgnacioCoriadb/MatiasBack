f4b95e74595c2a3b48a452dfce153e30e9390587
Icoriadb <icoriadebernardi@gmail.com>
2023-09-01 23:00:44 -0300

    prueba

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
-const server = require('./app');
+// const server = require('./app');
 // const PORT = 3001;
-const serverApp = require('./app'); // Importa el servidor Express
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const {sequelize} = require('./src/Database/db');
-const  {createUser} = require("./src/createUser/createUser");
-
-
-//?-----------Moldels------------
-const Images = require('./src/Models/Images');
-const Folders = require('./src/Models/Folders');
-const User = require('./src/Models/User');
-//?------------------------------
+// const {sequelize} = require('./src/Database/db');
+// const  {createUser} = require("./src/createUser/createUser");
+// //?-----------Moldels------------
+// const Images = require('./src/Models/Images');
+// const Folders = require('./src/Models/Folders');
+// const User = require('./src/Models/User');
+// //?------------------------------
 
 
 
-sequelize.sync({ force: false }).then(async() => await createUser())
+// sequelize.sync({ force: false }).then(async() => await createUser())
 
 
 // server.listen(PORT, () => console.log("SERVER IN PORT " + PORT));
 
-const httpServer  = http.createServer((req, res) => {
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hola mundo!\n'); //CAMBIAR POR SERVER const server = require('./app');
+    res.end('Hola mundo!\n');
 });
-httpServer.listen(port, hostname, () => {
+server.listen(port, hostname, () => {
     console.log(`Servidor corriendo en en http://${hostname}:${port}/`);
 });
