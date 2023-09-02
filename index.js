31a0ce71344c90c123d81cd1a85b21a038fb1107
Icoriadb <icoriadebernardi@gmail.com>
2023-09-01 22:38:00 -0300

    index

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const server = require('./app');
 // const PORT = 3001;
+const serverApp = require('./app'); // Importa el servidor Express
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
 const {sequelize} = require('./src/Database/db');
 const  {createUser} = require("./src/createUser/createUser");
+
+
 //?-----------Moldels------------
 const Images = require('./src/Models/Images');
 const Folders = require('./src/Models/Folders');
@@ -15,9 +22,6 @@ sequelize.sync({ force: false }).then(async() => await createUser())
 
 // server.listen(PORT, () => console.log("SERVER IN PORT " + PORT));
 
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
