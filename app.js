dd5c4bef88dbc02ee05468bec4e032c634a6a547
Icoriadb <icoriadebernardi@gmail.com>
2023-09-06 10:48:48 -0300

    cors config

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
 server.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://www.longoperrig.com.ar');
+    res.setHeader('Access-Control-Allow-Origin', 'http://vps-3582101-x.dattaweb.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
