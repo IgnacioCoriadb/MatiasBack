56c52371f76667a009327884da51cc82cdf28947
Icoriadb <icoriadebernardi@gmail.com>
2023-09-14 15:14:08 -0300

    sharp

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const server = require('./app');
 const PORT = 3001;
 const {sequelize} = require('./src/Database/db');
 const  {createUser} = require("./src/createUser/createUser");
-const fs = require('fs');
 //?-----------Moldels------------
 const Images = require('./src/Models/Images');
 const Folders = require('./src/Models/Folders');
@@ -11,14 +10,7 @@ const User = require('./src/Models/User');
 
 
 
-sequelize.sync({ force: false }).then(async() => await createUser());
+sequelize.sync({ force: false }).then(async() => await createUser())
 
-const options = {
-    key: fs.readFileSync('/etc/letsencrypt/live/vps-3582101-x.dattaweb.com/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/vps-3582101-x.dattaweb.com/fullchain.pem')
-  };
-  
 
-const serverRun = https.createServer(options, server);
-
-serverRun.listen(PORT, () => console.log("SERVER IN PORT " + PORT));
+server.listen(PORT, () => console.log("SERVER IN PORT " + PORT));
