87020679d8dadc5c40a0b02b4774d76978854fd3
Icoriadb <icoriadebernardi@gmail.com>
2023-09-27 10:00:35 -0300

    url front

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
 server.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://longoperrig.com.ar:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
